47a53c111d4ae7f5a69a092309b0705be2445ad0
muriloIFMS <murilo.oliveira4@estudante.ifms.edu.br>
2024-04-15 21:35:19 -0400

    Merge branch 'dev' of github.com-muriloIFMS:murilosoaresIFMS/capiBank into dev

--- Config/app.js
+++ Config/app.js
@@ -1,91 +1,91 @@
 const http = require('http')
 
-/* PÁGINAS FRONT END */
-const login_page = require('./pages/login/html.js')
-const register_page = require('./pages/cadastro/html.js')
-const dashboard = require('./pages/dashboard/html.js')
+// FUNÇÃO QUE LÊ OS ARQUIVOS DO FRONT-END
+const getFile = require('./scripts/getFile')
 
-/* SCRIPTS DE PROCESSAMENTO */
-const get_form_data = require('./scripts/pegar_dados.js')
-const save_data = require('./scripts/salvar_cadastro.js')
-const validate_login = require('./scripts/validar_login.js')
+// FUNÇÃO QUE CRIA TABELA
+const { addTable } = require('./database/database') 
 
-/* DATABASE */
-const { addTable } = require('./database/clientes.js') // Cria tabela dados_clientes
+// SCRIPTS INTERAÇÃO FORM X BANCO
+const getFormData = require('./scripts/getFormData')
+const saveData = require('./scripts/saveData')
+const validateData = require('./scripts/validateData')
+
+// BIBLIOTECA NODE PARA EXTRAIR DADOS DA URL
+var parse = require('url').parse
+
+// ARQUIVO CONFIG.JSON
+var config = require('./config.json')
+var pasta_root = config.rootFolder
+var inicial = config.defaultIndex
+var types = config.types
 
 const server = http.createServer((req, res) => {
 
-    // RENDERIZA AS PÁGINAS DE FRONT-END NO SERVIDOR
+    function renderPage() {
 
-    if (req.url === '/' || req.url === '/login.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(login_page)
-    }
+        var arquivo_front = parse(req.url).pathname, path, extension
 
-    else if (req.url == '/loginError.html') {
-        const result =
-            `
-            ${login_page}
-            <script>
-                let error_message = document.querySelector('.flash-error')
-                error_message.style.display = 'flex'
-            </script>`
-        res.end(result)
-    }
+        if (req.url == '/') {
+            arquivo_front = inicial
+        }
+        
+        path = pasta_root + arquivo_front
+        extension = path.substr(path.lastIndexOf('.') + 1);
 
-    else if (req.url === '/cadastro.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(register_page)
-    }
-
-    else if (req.url === '/dashboard.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(dashboard)
-    }
-
-    // LIDA COM AS REQUISIÇÕES DE UM FORMULÁRIO (ACTION)
-
-    else if (req.url === '/salvar_cadastro.js' || req.url === '/validar_login.js') {
-        // Função que recebe os dados de formulário
-        const form_data = get_form_data(req, (err, data) => {
-
-            if (err) {
-                return 'Erro ao processar o registro:' + error
-            }
-
-            else {
-
-                if (req.url === '/salvar_cadastro.js') {
-                    save_data(data)
-                    res.end('<h1> Cadastro realizado com sucesso! </h1> \n <a href = "login.html">Voltar</a>') // Mensagem de sucesso
-                }
-
-                else if (req.url === '/validar_login.js') {
-                    validate_login(data).then(login => {
-                        const redirectUrl = login ? '/dashboard.html' : '/loginError.html'; // Condicional que define a URL
-                        res.writeHead(302, { 'Location': redirectUrl });
-                        res.end();
-                    });
-
-                }
-            }
-
+        getFile(path, function (data) {
+            res.writeHead(200, { 'Content-Type': types[extension] || 'text/plain',});
+            res.end(data);
+        }, function (err) {
+            console.log(err)
+            res.writeHead(404);
+            res.end(); 
         });
     }
+   
+    if (req.url === '/salvar_cadastro.js' || req.url === '/validar_login.js') {
+        
+        const captureForm = getFormData(req, (err, data) => {
+            
+            if (err) {
+                return 'Erro ao processar o registro:' + err
+            }
 
+            let operation  = req.url == '/salvar_cadastro.js' ? saveData(data) : validateData(data)
+            let page = req.url == '/salvar_cadastro.js' ? 'cadastro.html' : 'login.html'
+
+            operation.then((result) => {
+            
+                if ((result) && page == 'login.html') {
+                    res.writeHead(302, { 'Location': 'dashboard.html' });
+                    res.end() 
+                }
+
+                else if ((result) && page == 'cadastro.html') {
+                    res.end('<h1> Cadastro efetuado! </h1> <a href = "login.html">Voltar</a>')
+                }
+
+                else  {
+                    res.writeHead(302, { 'Location': page });
+                    res.end()
+                    return 
+                } 
+
+            })
+
+        })
+    }
+    
     else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Página não encontrada');
+        renderPage()
     }
 
-});
-
-
-// Processo assíncrono.. A função addTable importada de database/clients tem como retorno um booleano que é True ou False. Se for false, ele impede que o servidor execute e encerra a função assíncrona.
+})
+       
 addTable().then(isConnected => {
 
     if (!isConnected) {
-        console.log('Não foi possível conectar ao banco de dados. O servidor não será iniciado.')
+        console.log('Não foi possível conectar ao banco de dados. O servidor não será iniciado. Certifique se as credenciais estão corretas.')
         return
     }
 
@@ -95,5 +95,3 @@ addTable().then(isConnected => {
     })
 
 })
-
-
